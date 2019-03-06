e87364b3be891e2921478802ba56e472716649d1
Lydia2010 <lidiya.s@gmail.com>
2019-03-06 12:31:08 -0500

    final submission.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,47 @@
 //Run jQuery after the document is fully loaded.
-$(document).ready(
-    //The function that does the stuff.
-    function () {
-        //Make the AJAX call
-        $.ajax('http://api.tvmaze.com/singlesearch/shows?q=the+magicians&embed=episodes', {
-            method: "GET",
-            dataType: "json"
-        })
-        //After the data comes back, use this function
-            .done(
-                function (data) {
-                    //Add the name
-                    $('#name').append(data.name);
-                    //Add the episodes
-                    data._embedded.episodes.forEach(function (episode) {
-                        $('#episodeList').append('<tr>'+
-                            '<td>' + episode.season + '</td>' +
-                            '<td>' + episode.number + '</td>' +
-                            '<td>' + episode.name + '</td>' +
-                            '<td>' + episode.summary + '</td>' +
-                            +' </tr>')
+$(document).ready(function() {
+    $('#submit-show').bind('click',
+        function (event) {
+            event.preventDefault()
+            let currentShow = document.getElementById("name1").value;
+            currentShow=currentShow.replace(/\s/g, "+");
+            console.log(currentShow);
+
+
+    $.ajax ('http://api.tvmaze.com/singlesearch/shows?q=' + currentgit Show + '&embed=episodes', {
+
+        method: "GET",
+        dataType: "json"
+
+    })
+
+    //After the data comes back, use this function
+        .done(
+            function (data) {
+                // Add the name
+                $("name").html("");
+                $('#name').html(data.name);
+                $("#episodeList").html("");
+                // Add the episodes
+                data._embedded.episodes.forEach(function (episode) {
+                    $('#episodeList').append('<tr>' +
+                        '<td>' + episode.season + '</td>' +
+                        '<td>' + episode.number + '</td>' +
+                        '<td>' + episode.name + '</td>' +
+                        '<td>' + episode.summary + '</td>' +
+                        +' </tr>')
+                })
             })
-        })
-    }
-)
+})
+
+
+
+
+
+})
+
+
+
+
+
+
